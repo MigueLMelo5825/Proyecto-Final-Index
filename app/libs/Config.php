4056f05cca1de1154112596ff437398c259f5e89
MigueLMelo5825 <miguelfdo5825@gmail.com>
2026-01-20 17:40:35 +0100

    se crea la conexion a la base datos /app/core/Database.php y la configuracion de la misma /app/libs/Config.php

---
+++ app/libs/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+//configuracion de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'index');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+
+//$mvc_vis_css = "estilo.css";
+//$vista = __DIR__ . '/../templates/inicio.php';
+//$menu = __DIR__ . '/../templates/menuInvitado.php';
+?>
