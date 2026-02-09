456a14c283311fcf90f0157a558f0c74a8caa543
proyectopersonal12345 <puso87287@gmail.com>
2026-02-09 18:22:07 +0100

    actualizado la foto de favicon y solucionado el problema de sql

--- app/templates/header.php
+++ app/templates/header.php
@@ -26,6 +26,7 @@ $logoUrl = $base_url . 'web/img/INDEX-02.png';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $base_url ?>web/css/styles.css">
+    <link rel="icon" type="image/png" href="web/img/INDEX-01_favicon.png">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
